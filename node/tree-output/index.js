7a766b70ccba61301320e9ab90e6ecfd87bdc570
Andrey Lukutkin <andrey.lukutkin@gmail.com>
2021-12-29 22:27:38 +0300

    Redundant code removed

--- node/tree-output/index.js
+++ node/tree-output/index.js
@@ -2,21 +2,19 @@ const data = require('./data.json')
 
 function objectToTree(obj) {
     function indentPrint(obj, nameIndent, itemsIndent) {
-        let ret = nameIndent + obj.name + '\n'
+        console.log(nameIndent + obj.name)
 
         obj.items?.forEach((el, index) => {
             const isLastElement = (index === obj.items.length - 1)
-            ret += indentPrint(
+            indentPrint(
                 el,
                 itemsIndent + (isLastElement ? '└── ' : '├── '),
                 itemsIndent + (isLastElement ? '    ' : '|   ')
             )
         })
-
-        return ret
     }
     
-    return indentPrint(obj, '', '')
+    indentPrint(obj, '', '')
 }
 
-console.log(objectToTree(data))
+objectToTree(data)
